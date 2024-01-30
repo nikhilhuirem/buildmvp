2ac8a8b049de1fa0cf0886ea0c3488280fe04cec
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-30 21:27:36 +0530

    project added

---
+++ apps/docs/app/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@repo/ui/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@repo/ui/components/ui/avatar"
+
+
+export default function Page() {
+  return (
+    <main>
+      <Button>Click me</Button>
+
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+
+    </main>
+  );
+}
