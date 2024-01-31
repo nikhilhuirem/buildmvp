141ff222ed6677b671da75d44294e4224d3b740a
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-31 14:32:04 +0530

    added navigation

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,17 +1,41 @@
 import { Button } from "@repo/ui/components/ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "@repo/ui/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "@repo/ui/components/ui/avatar";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@repo/ui/components/ui/navigation-menu";
+
+
 
 
 export default function Page() {
   return (
     <main>
-      <Button>Click me</Button>
-
+      <div className="flex justify-around gap-10 mt-3">
       <Avatar>
         <AvatarImage src="https://github.com/shadcn.png" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
-
+      <div>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      </div>
+      <Button>Click me</Button>
+      </div>
+      
     </main>
   );
 }
