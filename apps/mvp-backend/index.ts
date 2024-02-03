61175f9718349f4482507220d1fd35971dff4389
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-02-03 21:04:15 +0530

    added navbar section

--- apps/mvp-backend/index.ts
+++ apps/mvp-backend/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("hello World")
+})
 app.listen(8000, () => {
     console.log(`Server is running on port 8000`);
 })
